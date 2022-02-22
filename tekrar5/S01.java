54b4736ad06b0dfe42e02259f0a51ed76542c633
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-02-22 21:22:51 +0300

    Scanner

---
+++ tekrar5/S01.java
@@ -0,0 +1,31 @@
+package tekrar5;
+
+import java.util.Scanner;
+
+public class S01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Kullanicidan bir tamsayi girmesini isteyin ve sayiyi kendisinden bir kucuk
+		 * sayi ve bir buyuk sayi ile carpip sonucu yazdirin
+		 * 
+		 * Ornek Input  : 10 
+		 *       Output : 990 (9*10*11)
+		 */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Bir tam sayi giriniz... ");
+		
+		int sayi = scan.nextInt();
+		
+		
+		System.out.println((sayi+1)*(sayi)*(sayi-1)+" ("+ (sayi-1)+ "*"+sayi+"*"+(sayi+1)+")");
+
+		
+		scan.close();
+
+	}
+
+}
